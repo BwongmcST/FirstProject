f1ed5cd9b859fdb7ffe4e47deda67fa6f93e2581
BwongmcST <ben.manchunwong@gmail.com>
2022-02-01 21:13:33 +0000

    edited test cases

--- src/test/java/com/sparta/firstProjectTest/BubbleSorterTest.java
+++ src/test/java/com/sparta/firstProjectTest/BubbleSorterTest.java
@@ -1,8 +1,10 @@
 package com.sparta.firstProjectTest;
 
 import com.sparta.firstProject.model.BubbleSorter;
+import com.sparta.firstProject.model.MergeSorter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,14 +12,34 @@ import java.util.Arrays;
 public class BubbleSorterTest {
 
     @Test
-        public void testBubbleSorter(){
-            BubbleSorter bubbleSorter = new BubbleSorter();
-            int[] testArray = {1,61,33,13,89,0,60};
-            bubbleSorter.sort(testArray);
+    @DisplayName("Regular input test")
+    public void regularInputTest() {
+        BubbleSorter bubbleSorter = new BubbleSorter();
+        int[] testArray = {1, 61, 33, 13, 89, 0, 60};
+        int[] expectedArray = {1, 61, 33, 13, 89, 0, 60};
 
-            String actual = Arrays.toString(testArray);
-            String expected = "[0, 1, 13, 33, 60, 61, 89]";
-            Assertions.assertEquals(actual,expected);
+        bubbleSorter.sort(testArray);
+        Arrays.sort(expectedArray);
+
+        String actual = Arrays.toString(testArray);
+        String expected = Arrays.toString(expectedArray);
+
+        Assertions.assertEquals(actual, expected);
+    }
+    @Test
+    @DisplayName("Multiple zero test")
+    public void multipleZeroTest() {
+        BubbleSorter bubbleSorter = new BubbleSorter();
+        int[] testArray = {0, 0, 11, 0, 0, 0, 2};
+        int[] expectedArray = {0, 0, 11, 0, 0, 0, 2};
+
+        bubbleSorter.sort(testArray);
+        Arrays.sort(expectedArray);
+
+        String actual = Arrays.toString(testArray);
+        String expected = Arrays.toString(expectedArray);
+
+        Assertions.assertEquals(actual, expected);
     }
 }
 
