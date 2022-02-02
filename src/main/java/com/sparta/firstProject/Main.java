fca68ddf8f8ef508e8e64c383d388cb71433aea3
BwongmcST <ben.manchunwong@gmail.com>
2022-02-02 14:28:09 +0000

    added log4j logfile

--- src/main/java/com/sparta/firstProject/Main.java
+++ src/main/java/com/sparta/firstProject/Main.java
@@ -2,17 +2,20 @@ package com.sparta.firstProject;
 
 import com.sparta.firstProject.controller.SortController;
 import com.sparta.firstProject.view.SortView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
+    public static Logger logger = LogManager.getLogger("Sorter Logger");
 
     public static void main(String[] args) {
 
+
+
         //int[] testArrayBubble = Arrays.copyOf(testArray, testArray.length);
         //int[] testArrayMerge = Arrays.copyOf(testArray, testArray.length);
-
+        logger.info("Start sorting application");
         SortView sortView = new SortView();
         int[] intArray = sortView.getIntArray();
         String sortMethod = sortView.getSortMethod();
