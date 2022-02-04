b1d0a7fd8d90ebaf54ac263b9357cd3ef66ef9fb
BwongmcST <ben.manchunwong@gmail.com>
2022-02-04 00:17:51 +0000

    re-create generateInt method, without repeat value

--- src/main/java/com/sparta/firstProject/model/RandomIntGenerator.java
+++ src/main/java/com/sparta/firstProject/model/RandomIntGenerator.java
@@ -1,12 +1,37 @@
 package com.sparta.firstProject.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static com.sparta.firstProject.main.Main.logger;
 
 public class RandomIntGenerator {
 
+public int[] generateInt(int arrayLength, int min, int max) {
+    if(min >max){
+        logger.warn("Invalid value: "+ min +" >" + max);
+        return null;
+    }
+    //int[] intArray = new int[arrayLength];
+    List<Integer> integerList = new ArrayList<>();
+    int minVal = min;
+    int maxVal = max;
 
+    for(int i = minVal;i < maxVal +1; i++){
+        integerList.add(i);
+    }
+    Collections.shuffle(integerList);
+    int[] outputArray = new int[arrayLength];
+    for(int i = 0; i< arrayLength;i++){
+        outputArray[i] = integerList.get(i);
+    }
+    return outputArray;
+}
+
+// Old version (random with repeated value)
+/*
     public int[] generateInt(int arrayLength, int min, int max) {
         if(min >max){
             logger.warn("Invalid value: "+ min +" >" + max);
@@ -22,6 +47,10 @@ public class RandomIntGenerator {
         }
         return intArray;
     }
+ */
+
+
+
 
 
 
